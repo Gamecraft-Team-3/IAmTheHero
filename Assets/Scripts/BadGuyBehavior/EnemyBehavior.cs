0731e2a9e8f7ce38a782c536a702f3f39661b91a
AlmityTuhm <driggsthomas@gmail.com>
2023-02-18 11:28:51 -0700

    Bad Guys

--- Assets/Scripts/BadGuyBehavior/EnemyBehavior.cs
+++ Assets/Scripts/BadGuyBehavior/EnemyBehavior.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    private Vector3 goal;
+    private EnemySpawner enemySpawner;
+    private float distanceX;
+    private float distanceY;
+    [SerializeField] private int speed = 3;
+    [SerializeField] private int health = 3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemySpawner = GameObject.Find("Enemy Manager").GetComponent<EnemySpawner>();
+        goal = GameObject.Find("Goal").transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        distanceX = Mathf.Abs(this.transform.position.x - goal.x);
+        distanceY = Mathf.Abs(this.transform.position.y - goal.y);
+
+        transform.LookAt(goal);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if(distanceX <= 1.0f && distanceY <= 1.0f)
+        {
+            ReachGoal();
+        }
+    }
+
+    private void ReachGoal()
+    {
+        Debug.Log("Hamburger");
+        enemySpawner.RemoveBadGuy(this.gameObject);
     }
 }
--- Assets/Scripts/BadGuyBehavior/EnemySpawner.cs
+++ Assets/Scripts/BadGuyBehavior/EnemySpawner.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> badGuys = new List<GameObject>();
+    [SerializeField] private GameObject badGuyPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SummonBadGuy();
+        SummonBadGuy();
+        SummonBadGuy();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //Summons the Bad Guy at a random location
+    private void SummonBadGuy()
+    {
+        GameObject instance = Instantiate(badGuyPrefab);
+        instance.transform.position = GetRandomPosition();
+        badGuys.Add(instance);        
+    }
+
+    //Removes a bad guy from the list and destroys it
+    public void RemoveBadGuy(GameObject badGuy)
+    {
+        badGuys.Remove(badGuy);
+        Destroy(badGuy);
+    }
+
+    //Returns a random Vector3 for a position
+    private Vector3 GetRandomPosition()
+    {
+        float xPos = Random.Range(0, 5);
+        float yPos = 0.0f;
+        float zPos = Random.Range(0, 5);
+
+        return new Vector3(xPos, yPos, zPos);
+
     }
 }
