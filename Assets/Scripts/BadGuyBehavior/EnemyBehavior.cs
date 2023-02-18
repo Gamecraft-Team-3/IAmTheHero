29b3695bf1c0506a1885ab495f0ed1f0b08950df
AlmityTuhm <driggsthomas@gmail.com>
2023-02-18 12:08:20 -0700

    BetterBehavior

--- Assets/Scripts/BadGuyBehavior/EnemyBehavior.cs
+++ Assets/Scripts/BadGuyBehavior/EnemyBehavior.cs
@@ -8,13 +8,19 @@ public class EnemyBehavior : MonoBehaviour
     private EnemySpawner enemySpawner;
     private float distanceX;
     private float distanceY;
-    [SerializeField] private int speed = 3;
+    [SerializeField] private float speed = 3;
     [SerializeField] private int health = 3;
     // Start is called before the first frame update
     void Start()
     {
+        //Initialize Enemy Spawner
         enemySpawner = GameObject.Find("Enemy Manager").GetComponent<EnemySpawner>();
+        //Initialize their individual goal
         goal = GameObject.Find("Goal").transform.position;
+        goal.z += 1;
+        goal.z *= Random.Range(-5, 5);
+        //SPEED
+        speed *= Random.Range(0.9f, 1.2f);
     }
 
     // Update is called once per frame
@@ -29,11 +35,20 @@ public class EnemyBehavior : MonoBehaviour
         {
             ReachGoal();
         }
+        if(health <= 0)
+        {
+            BadGuyDies();
+        }
     }
 
     private void ReachGoal()
     {
-        Debug.Log("Hamburger");
+        enemySpawner.RemoveBadGuy(this.gameObject);
+        //Cause damage to the "Player"
+    }
+
+    private void BadGuyDies()
+    {
         enemySpawner.RemoveBadGuy(this.gameObject);
     }
 }
--- Assets/Scripts/BadGuyBehavior/EnemySpawner.cs
+++ Assets/Scripts/BadGuyBehavior/EnemySpawner.cs
@@ -6,12 +6,14 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private List<GameObject> badGuys = new List<GameObject>();
     [SerializeField] private GameObject badGuyPrefab;
+
+    private int waveCount = 0;
+    private int enemyCount = 0;
+    private bool isFinalWave = false;
     // Start is called before the first frame update
     void Start()
     {
-        SummonBadGuy();
-        SummonBadGuy();
-        SummonBadGuy();
+        StartCoroutine(WaveCountdown());
     }
 
     // Update is called once per frame
@@ -20,12 +22,46 @@ public class EnemySpawner : MonoBehaviour
         
     }
 
+    //Waits a set amount of time before each wave.
+    private IEnumerator WaveCountdown()
+    {
+        DetermineEnemyCount();
+        yield return new WaitForSeconds(5.0f);
+        StartCoroutine(SummonWave());
+    }
+
+    //Summons a wave of enemies.  The amount is determined by another function
+    private IEnumerator SummonWave()
+    {
+
+        for(int i = 0; i < enemyCount; i++)
+        {
+            yield return new WaitForSeconds(0.1f);
+            SummonBadGuy();
+        }
+        if (isFinalWave)
+        {
+            SummonBoss();
+            StopCoroutine(WaveCountdown());
+        }
+        else
+        {
+            StartCoroutine(WaveCountdown());
+        }
+    }
+
     //Summons the Bad Guy at a random location
     private void SummonBadGuy()
     {
         GameObject instance = Instantiate(badGuyPrefab);
         instance.transform.position = GetRandomPosition();
         badGuys.Add(instance);        
+    }
+
+    //Summons the big bad guy
+    private void SummonBoss()
+    {
+        Debug.Log("Hey Stinky");
     }
 
     //Removes a bad guy from the list and destroys it
@@ -38,11 +74,30 @@ public class EnemySpawner : MonoBehaviour
     //Returns a random Vector3 for a position
     private Vector3 GetRandomPosition()
     {
-        float xPos = Random.Range(0, 5);
+        float xPos = Random.Range(-5, 5);
         float yPos = 0.0f;
-        float zPos = Random.Range(0, 5);
+        float zPos = Random.Range(-5, 5);
 
         return new Vector3(xPos, yPos, zPos);
 
+    }
+
+    //Determines how many enemies will spawn depending on the wave
+    private void DetermineEnemyCount()
+    {
+        waveCount++;
+        if(waveCount == 1)
+        {
+            enemyCount = 7;
+        }
+        else if(waveCount == 2)
+        {
+            enemyCount = 18;
+        }
+        else if(waveCount == 3)
+        {
+            enemyCount = 10;
+            isFinalWave = true;
+        }
     }
 }
